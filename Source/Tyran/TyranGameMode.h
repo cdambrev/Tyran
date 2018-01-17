f936f103403aefade55bc22566708cb1ddccf92e
Cyril Richard <ricc1905@usherbrooke.ca>
2018-01-17 13:37:02 -0500

    Creation Projet Third Person

---
+++ Source/Tyran/TyranGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TyranGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATyranGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATyranGameMode();
+};
+
+
+
