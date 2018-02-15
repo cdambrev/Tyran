1089e85469bc303cafcc99dc0f705bd111acacfe
Maxime Taisant <maximetaisant@hotmail.fr>
2018-02-15 11:18:16 -0500

    Ajout d'un surlignage autour du loot en focus

--- Source/Tyran/Basic/TyranCharacter.cpp
+++ Source/Tyran/Basic/TyranCharacter.cpp
@@ -155,8 +155,6 @@ ALoot * ATyranCharacter::GetLootInView()
 	const FVector Direction = CamRot.Vector(); 
 	const FVector TraceEnd = TraceStart + (Direction * MaxUseDistance); 
 	
-	//FCollisionObjectQueryParams objectQueryParams{};
-	//objectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
 	FCollisionQueryParams TraceParams(FName(TEXT("TraceLoot")), true, this); 
 	TraceParams.bTraceAsyncScene = true; 
 	TraceParams.bReturnPhysicalMaterial = false; 
@@ -164,13 +162,9 @@ ALoot * ATyranCharacter::GetLootInView()
 	//TraceParams.AddIgnoredActor(this);
 	
 	FHitResult Hit(ForceInit);
-	bool succes = GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, ECC_Camera, TraceParams);
-	//bool succes = GetWorld()->LineTraceSingleByObjectType(Hit, TraceStart, TraceEnd, objectQueryParams, TraceParams);
-	if (succes)
-		bool b = true;
+	bool succes = GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, ECC_Visibility, TraceParams);
 	
-	// Cette ligne sera en commentaire plus tard 
-	DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Red, false, 1.0f); 
+	//DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Red, false, 1.0f); 
 	
 	return Cast<ALoot>(Hit.GetActor());
 }
